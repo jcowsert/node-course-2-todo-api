bb7292edde2d10e68eb4f84e90d935d67b2945ca
Joseph Cowsert <cowsertjoe@gmail.com>
2018-07-13 13:55:30 -0500

    added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const{User} = require('./../server/models/user');
+
+// var id = '5b48e1a4cb146f9c3a44a8067';
+var id = '5b47a41bc4094bbc0cea833b'
+//
+// if (!ObjectID.isValid(id)){
+//     console.log('Id not valid');
+// }
+// Todo.find({
+//     _id: id
+// }).then((todos) =>{
+//     console.log('Todos', todos)
+// });
+//
+// Todo.findOne({
+//     _id: id
+// }).then((todo) =>{
+//     console.log('Todo', todo)
+// });
+
+// Todo.findById(id).then((todo) =>{
+//     if(!todo){
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id', todo)
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user)=> {
+    if(!user){
+        return console.log('User Id not found');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
